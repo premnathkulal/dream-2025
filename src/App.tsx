ea971161ad0cb5cd20091a8b8517f01ff256ff85
Premnath K <premnathkulal1998@gmail.com>
2024-12-30 21:08:34 +0530

    Created header and footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import { Outlet } from "react-router-dom";
 import "./App.css";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
     <div className="app">
+      <Header />
       <Outlet />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/header/Header.tsx
@@ -0,0 +1,33 @@
+import "./Header.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSignOut, faUser } from "@fortawesome/free-solid-svg-icons";
+import BrandLogo from "../../assets/react.svg";
+import { useState } from "react";
+
+const Header = () => {
+  const [isDropdownVisible, setDropdownVisible] = useState(false);
+
+  return (
+    <div className="header">
+      <div className="brand-logo">
+        <img src={BrandLogo} alt="brand-logo" className="brand-logo-img" />
+      </div>
+      <div className="brand-name">Shenoy Enterprises</div>
+      <div
+        className="auth-logo"
+        onMouseEnter={() => setDropdownVisible(true)}
+        onMouseLeave={() => setDropdownVisible(false)}
+      >
+        <FontAwesomeIcon icon={faUser} className="auth-logo-icon" />
+        {isDropdownVisible && (
+          <div className="dropdown">
+            <div className="dropdown-item">Logout</div>
+            <FontAwesomeIcon icon={faSignOut} className="auth-action-icon" />
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Header;
